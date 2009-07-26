24b9ca28e6b0ecfca3c457b7a501470b753979e1
v.ripoll <v.ripoll@e025e472-b324-11dd-8bee-0be8dff76351>
2009-07-26 19:33:49 +0000

    Mise à jour de la base de données pour Port-Réal + passage à la version suivante (changement de format de base : MinimalAgoTDBVersion devient MinimalApplicationVersion) + gestion des styles encapsulant des sections de texte vides

--- trunk/GenericDB/GenericDBTestProject/DatabaseManagerTest.cs
+++ trunk/GenericDB/GenericDBTestProject/DatabaseManagerTest.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Drawing;
 using GenericDB.DataAccess;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -68,14 +67,14 @@ namespace GenericDBTestProject
 
 			var actual = DatabaseManager_Accessor.ExtractFormattedStringValueFromRow(row, columnName);
 
-			Assert.IsTrue(actual.Formats.Count == 0);
-			Assert.IsTrue(actual.Value == "Hello world");
+			Assert.AreEqual(0, actual.Formats.Count);
+			Assert.AreEqual("Hello world", actual.Value);
 
 			actual = DatabaseManager_Accessor.ExtractFormattedStringValueFromRow(row, columnName,
 				new BoundFormat('{', '}', new TextFormat("errata", Color.Red)));
 
-			Assert.IsTrue(actual.Formats.Count == 0);
-			Assert.IsTrue(actual.Value == "Hello world");
+			Assert.AreEqual(0, actual.Formats.Count);
+			Assert.AreEqual("Hello world", actual.Value);
 		}
 
 		/// <summary>
@@ -95,8 +94,8 @@ namespace GenericDBTestProject
 
 			var actual = DatabaseManager_Accessor.ExtractFormattedStringValueFromRow(row, columnName);
 
-			Assert.IsTrue(actual.Formats.Count == 0);
-			Assert.IsTrue(actual.Value == "Hello {world}, the ~sun~ is shinning");
+			Assert.AreEqual(0, actual.Formats.Count);
+			Assert.AreEqual("Hello {world}, the ~sun~ is shinning", actual.Value);
 
 			var errataFormat = new TextFormat("errata", Color.Red);
 			var specialFormat = new TextFormat("special", Color.Green);
@@ -105,15 +104,54 @@ namespace GenericDBTestProject
 				new BoundFormat('{', '}', errataFormat),
 				new BoundFormat('~', specialFormat));
 
-			Assert.IsTrue(actual.Formats.Count == 2);
-			Assert.IsTrue(actual.Value == "Hello world, the sun is shinning");
-			Assert.IsTrue(actual.Formats[0].Begin == 6);
-			Assert.IsTrue(actual.Formats[0].End == 11);
-			Assert.IsTrue(actual.Formats[0].Format == errataFormat);
-			Assert.IsTrue(actual.Formats[1].Begin == 17);
-			Assert.IsTrue(actual.Formats[1].End == 20);
-			Assert.IsTrue(actual.Formats[1].Format == specialFormat);
+			Assert.AreEqual(2, actual.Formats.Count);
+			Assert.AreEqual("Hello world, the sun is shinning", actual.Value);
+			Assert.AreEqual(6, actual.Formats[0].Begin);
+			Assert.AreEqual(11, actual.Formats[0].End);
+			Assert.AreEqual(errataFormat, actual.Formats[0].Format);
+			Assert.AreEqual(17, actual.Formats[1].Begin);
+			Assert.AreEqual(20, actual.Formats[1].End);
+			Assert.AreEqual(specialFormat, actual.Formats[1].Format);
 		}
+
+		/// <summary>
+		///A test for ExtractFormattedStringValueFromRow
+		///</summary>
+		[TestMethod()]
+		public void ExtractFormattedStringValueFromRowTest_WithFormatAndEmptySection()
+		{
+			string columnName = "col";
+			var table = new DataTable("test");
+			var textColumn = new DataColumn(columnName);
+			textColumn.DataType = typeof(string);
+			table.Columns.Add(textColumn);
+
+			var row = table.NewRow();
+			row[columnName] = "Hello {world}, {}the~~ ~sun~ is shinning";
+
+			var errataFormat = new TextFormat("errata", Color.Red);
+			var specialFormat = new TextFormat("special", Color.Green);
+
+			var actual = DatabaseManager_Accessor.ExtractFormattedStringValueFromRow(row, columnName,
+				new BoundFormat('{', '}', errataFormat),
+				new BoundFormat('~', specialFormat));
+
+			Assert.AreEqual(4, actual.Formats.Count);
+			Assert.AreEqual("Hello world, {}the~~ sun is shinning", actual.Value);
+			Assert.AreEqual(6, actual.Formats[0].Begin);
+			Assert.AreEqual(11, actual.Formats[0].End);
+			Assert.AreEqual(errataFormat, actual.Formats[0].Format);
+			Assert.AreEqual(13, actual.Formats[1].Begin);
+			Assert.AreEqual(15, actual.Formats[1].End);
+			Assert.AreEqual(errataFormat, actual.Formats[1].Format);
+			Assert.AreEqual(18, actual.Formats[2].Begin);
+			Assert.AreEqual(20, actual.Formats[2].End);
+			Assert.AreEqual(specialFormat, actual.Formats[2].Format);
+			Assert.AreEqual(21, actual.Formats[3].Begin);
+			Assert.AreEqual(24, actual.Formats[3].End);
+			Assert.AreEqual(specialFormat, actual.Formats[3].Format);
+		}
+
 
 		/// <summary>
 		///A test for ExtractFormattedStringValueFromRow
@@ -137,17 +175,17 @@ namespace GenericDBTestProject
 				new BoundFormat('{', '}', errataFormat),
 				new BoundFormat('~', specialFormat));
 
-			Assert.IsTrue(actual.Formats.Count == 3);
-			Assert.IsTrue(actual.Value == "Hello world, the sun is shinning");
-			Assert.IsTrue(actual.Formats[0].Begin == 6);
-			Assert.IsTrue(actual.Formats[0].End == 23);
-			Assert.IsTrue(actual.Formats[0].Format == errataFormat);
-			Assert.IsTrue(actual.Formats[1].Begin == 17);
-			Assert.IsTrue(actual.Formats[1].End == 20);
-			Assert.IsTrue(actual.Formats[1].Format == specialFormat);
-			Assert.IsTrue(actual.Formats[2].Begin == 18);
-			Assert.IsTrue(actual.Formats[2].End == 19);
-			Assert.IsTrue(actual.Formats[2].Format == errataFormat);
+			Assert.AreEqual(3, actual.Formats.Count);
+			Assert.AreEqual("Hello world, the sun is shinning", actual.Value);
+			Assert.AreEqual(6, actual.Formats[0].Begin);
+			Assert.AreEqual(23, actual.Formats[0].End);
+			Assert.AreEqual(errataFormat, actual.Formats[0].Format);
+			Assert.AreEqual(17, actual.Formats[1].Begin);
+			Assert.AreEqual(20, actual.Formats[1].End);
+			Assert.AreEqual(specialFormat, actual.Formats[1].Format);
+			Assert.AreEqual(18, actual.Formats[2].Begin);
+			Assert.AreEqual(19, actual.Formats[2].End);
+			Assert.AreEqual(errataFormat, actual.Formats[2].Format);
 		}
 	}
 }
