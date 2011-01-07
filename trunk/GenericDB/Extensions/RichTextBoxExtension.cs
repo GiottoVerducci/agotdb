1d1ab8fe3e736b175d3535c01952db2e30a166b9
v.ripoll@gmail.com <v.ripoll@gmail.com@e025e472-b324-11dd-8bee-0be8dff76351>
2011-01-07 17:04:36 +0000

    Added some missing disclaimers. Added the RichTextBoxExtension method BeginUpdate and EndUpdate for Rich Text Boxes.

--- trunk/GenericDB/Extensions/RichTextBoxExtension.cs
+++ trunk/GenericDB/Extensions/RichTextBoxExtension.cs
@@ -15,26 +15,24 @@
 // You can contact me at v.ripoll@gmail.com
 
 using System;
-using Beyond.ExtendedControls;
+using System.Windows.Forms;
 
 namespace GenericDB.Extensions
 {
-	public static class ExtendedCheckedListBoxExtension
+	public static class RichTextBoxExtension
 	{
-		/// <summary>
-		/// Performs an action on the expanded items of the extended checked list box control
-		/// by de-condensing it and putting it back to its original state afterwards.
-		/// </summary>
-		/// <param name="ecl">The extended checked list box control.</param>
-		/// <param name="actionToPerform">The action to perform on the extended checked list box control once it is expanded.</param>
-		public static void WorkOnExpandedItems(this ExtendedCheckedListBox ecl, Action<ExtendedCheckedListBox> actionToPerform)
+		[System.Runtime.InteropServices.DllImport("user32.dll")]
+		private extern static IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+
+		public static void BeginUpdate(this RichTextBox rtb)
 		{
-			bool condensed = ecl.Condensed; // we'll use it to restore its previous state
-			if (condensed)
-				ecl.Condensed = false; // expand ecl to get access to the items
-			actionToPerform(ecl);
-			if (condensed)
-				ecl.Condensed = true; // restore condensed state
+			SendMessage(rtb.Handle, 0xb, (IntPtr)0, IntPtr.Zero);
+		}
+
+		public static void EndUpdate(this RichTextBox rtb)
+		{
+			SendMessage(rtb.Handle, 0xb, (IntPtr)1, IntPtr.Zero);
+			rtb.Invalidate();
 		}
 	}
 }
