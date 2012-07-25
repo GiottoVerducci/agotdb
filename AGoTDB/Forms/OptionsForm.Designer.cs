4a7b270f26a770fd0912c8a71eb7e02b66f9ae2d
v.ripoll@gmail.com <v.ripoll@gmail.com@e025e472-b324-11dd-8bee-0be8dff76351>
2012-07-25 10:12:12 +0000

    v. 0.739 Localized and fixed options form.

--- AGoTDB/Forms/OptionsForm.Designer.cs
+++ AGoTDB/Forms/OptionsForm.Designer.cs
@@ -28,153 +28,116 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OptionsForm));
 			this.cbDisplayImages = new System.Windows.Forms.CheckBox();
 			this.cbRebuildDatabase = new System.Windows.Forms.CheckBox();
 			this.btnOK = new System.Windows.Forms.Button();
 			this.btnCancel = new System.Windows.Forms.Button();
 			this.cbDisplayMessages = new System.Windows.Forms.CheckBox();
 			this.gbDisplay = new System.Windows.Forms.GroupBox();
-			this.gbMisc = new System.Windows.Forms.GroupBox();
 			this.lbCardPreviewSize = new System.Windows.Forms.Label();
 			this.nudCardPreviewSize = new System.Windows.Forms.NumericUpDown();
-			this.pnlCardPreviewSize = new System.Windows.Forms.Panel();
+			this.gbMisc = new System.Windows.Forms.GroupBox();
+			this.cbCheckForDatabaseUpdateOnStartup = new System.Windows.Forms.CheckBox();
 			this.gbDisplay.SuspendLayout();
-			this.gbMisc.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.nudCardPreviewSize)).BeginInit();
-			this.pnlCardPreviewSize.SuspendLayout();
+			this.gbMisc.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// cbDisplayImages
 			// 
-			this.cbDisplayImages.AutoSize = true;
-			this.cbDisplayImages.Location = new System.Drawing.Point(17, 24);
+			resources.ApplyResources(this.cbDisplayImages, "cbDisplayImages");
 			this.cbDisplayImages.Name = "cbDisplayImages";
-			this.cbDisplayImages.Size = new System.Drawing.Size(215, 17);
-			this.cbDisplayImages.TabIndex = 0;
-			this.cbDisplayImages.Text = "Display images (icons and card preview)";
 			this.cbDisplayImages.UseVisualStyleBackColor = true;
 			this.cbDisplayImages.CheckedChanged += new System.EventHandler(this.cbDisplayImages_CheckedChanged);
 			// 
 			// cbRebuildDatabase
 			// 
-			this.cbRebuildDatabase.AutoSize = true;
-			this.cbRebuildDatabase.Location = new System.Drawing.Point(17, 28);
+			resources.ApplyResources(this.cbRebuildDatabase, "cbRebuildDatabase");
 			this.cbRebuildDatabase.Name = "cbRebuildDatabase";
-			this.cbRebuildDatabase.Size = new System.Drawing.Size(170, 17);
-			this.cbRebuildDatabase.TabIndex = 1;
-			this.cbRebuildDatabase.Text = "Rebuild database on next start";
 			this.cbRebuildDatabase.UseVisualStyleBackColor = true;
 			// 
 			// btnOK
 			// 
-			this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			resources.ApplyResources(this.btnOK, "btnOK");
 			this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.btnOK.Location = new System.Drawing.Point(197, 202);
 			this.btnOK.Name = "btnOK";
-			this.btnOK.Size = new System.Drawing.Size(75, 23);
-			this.btnOK.TabIndex = 2;
-			this.btnOK.Text = "OK";
 			this.btnOK.UseVisualStyleBackColor = true;
 			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
 			// 
 			// btnCancel
 			// 
-			this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			resources.ApplyResources(this.btnCancel, "btnCancel");
 			this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btnCancel.Location = new System.Drawing.Point(107, 202);
 			this.btnCancel.Name = "btnCancel";
-			this.btnCancel.Size = new System.Drawing.Size(75, 23);
-			this.btnCancel.TabIndex = 3;
-			this.btnCancel.Text = "Cancel";
 			this.btnCancel.UseVisualStyleBackColor = true;
 			// 
 			// cbDisplayMessages
 			// 
-			this.cbDisplayMessages.AutoSize = true;
-			this.cbDisplayMessages.Location = new System.Drawing.Point(17, 73);
+			resources.ApplyResources(this.cbDisplayMessages, "cbDisplayMessages");
 			this.cbDisplayMessages.Name = "cbDisplayMessages";
-			this.cbDisplayMessages.Size = new System.Drawing.Size(123, 17);
-			this.cbDisplayMessages.TabIndex = 4;
-			this.cbDisplayMessages.Text = "Display all messages";
 			this.cbDisplayMessages.ThreeState = true;
 			this.cbDisplayMessages.UseVisualStyleBackColor = true;
 			// 
 			// gbDisplay
 			// 
-			this.gbDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.gbDisplay.Controls.Add(this.pnlCardPreviewSize);
+			resources.ApplyResources(this.gbDisplay, "gbDisplay");
+			this.gbDisplay.Controls.Add(this.lbCardPreviewSize);
+			this.gbDisplay.Controls.Add(this.nudCardPreviewSize);
 			this.gbDisplay.Controls.Add(this.cbDisplayImages);
 			this.gbDisplay.Controls.Add(this.cbDisplayMessages);
-			this.gbDisplay.Location = new System.Drawing.Point(12, 12);
 			this.gbDisplay.Name = "gbDisplay";
-			this.gbDisplay.Size = new System.Drawing.Size(260, 101);
-			this.gbDisplay.TabIndex = 5;
 			this.gbDisplay.TabStop = false;
-			this.gbDisplay.Text = "Display";
-			// 
-			// gbMisc
-			// 
-			this.gbMisc.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
-			this.gbMisc.Controls.Add(this.cbRebuildDatabase);
-			this.gbMisc.Location = new System.Drawing.Point(12, 124);
-			this.gbMisc.Name = "gbMisc";
-			this.gbMisc.Size = new System.Drawing.Size(260, 63);
-			this.gbMisc.TabIndex = 6;
-			this.gbMisc.TabStop = false;
-			this.gbMisc.Text = "Miscellaneous";
 			// 
 			// lbCardPreviewSize
 			// 
-			this.lbCardPreviewSize.AutoSize = true;
-			this.lbCardPreviewSize.Location = new System.Drawing.Point(3, 2);
+			resources.ApplyResources(this.lbCardPreviewSize, "lbCardPreviewSize");
 			this.lbCardPreviewSize.Name = "lbCardPreviewSize";
-			this.lbCardPreviewSize.Size = new System.Drawing.Size(110, 13);
-			this.lbCardPreviewSize.TabIndex = 5;
-			this.lbCardPreviewSize.Text = "Card preview size (%):";
 			// 
 			// nudCardPreviewSize
 			// 
 			this.nudCardPreviewSize.Increment = new decimal(new int[] {
-            25,
-            0,
-            0,
-            0});
-			this.nudCardPreviewSize.Location = new System.Drawing.Point(119, 0);
+			25,
+			0,
+			0,
+			0});
+			resources.ApplyResources(this.nudCardPreviewSize, "nudCardPreviewSize");
 			this.nudCardPreviewSize.Name = "nudCardPreviewSize";
-			this.nudCardPreviewSize.Size = new System.Drawing.Size(38, 20);
-			this.nudCardPreviewSize.TabIndex = 6;
 			// 
-			// pnlCardPreviewSize
+			// gbMisc
 			// 
-			this.pnlCardPreviewSize.Controls.Add(this.lbCardPreviewSize);
-			this.pnlCardPreviewSize.Controls.Add(this.nudCardPreviewSize);
-			this.pnlCardPreviewSize.Location = new System.Drawing.Point(32, 47);
-			this.pnlCardPreviewSize.Name = "pnlCardPreviewSize";
-			this.pnlCardPreviewSize.Size = new System.Drawing.Size(155, 20);
-			this.pnlCardPreviewSize.TabIndex = 7;
+			resources.ApplyResources(this.gbMisc, "gbMisc");
+			this.gbMisc.Controls.Add(this.cbCheckForDatabaseUpdateOnStartup);
+			this.gbMisc.Controls.Add(this.cbRebuildDatabase);
+			this.gbMisc.Name = "gbMisc";
+			this.gbMisc.TabStop = false;
+			// 
+			// cbCheckForDatabaseUpdateOnStartup
+			// 
+			resources.ApplyResources(this.cbCheckForDatabaseUpdateOnStartup, "cbCheckForDatabaseUpdateOnStartup");
+			this.cbCheckForDatabaseUpdateOnStartup.Name = "cbCheckForDatabaseUpdateOnStartup";
+			this.cbCheckForDatabaseUpdateOnStartup.UseVisualStyleBackColor = true;
 			// 
 			// OptionsForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			resources.ApplyResources(this, "$this");
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 237);
 			this.Controls.Add(this.gbMisc);
 			this.Controls.Add(this.gbDisplay);
 			this.Controls.Add(this.btnCancel);
 			this.Controls.Add(this.btnOK);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
 			this.Name = "OptionsForm";
-			this.Text = "OptionsForm";
+			this.ShowInTaskbar = false;
 			this.Load += new System.EventHandler(this.OptionsForm_Load);
 			this.Shown += new System.EventHandler(this.OptionsForm_Shown);
 			this.gbDisplay.ResumeLayout(false);
 			this.gbDisplay.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.nudCardPreviewSize)).EndInit();
 			this.gbMisc.ResumeLayout(false);
 			this.gbMisc.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.nudCardPreviewSize)).EndInit();
-			this.pnlCardPreviewSize.ResumeLayout(false);
-			this.pnlCardPreviewSize.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -190,6 +153,6 @@
 		private System.Windows.Forms.GroupBox gbMisc;
 		private System.Windows.Forms.NumericUpDown nudCardPreviewSize;
 		private System.Windows.Forms.Label lbCardPreviewSize;
-		private System.Windows.Forms.Panel pnlCardPreviewSize;
+		private System.Windows.Forms.CheckBox cbCheckForDatabaseUpdateOnStartup;
 	}
 }
