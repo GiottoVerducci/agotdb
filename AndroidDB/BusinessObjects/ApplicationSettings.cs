8e0fad53054737003c0755a05161cde6c70eb2c3
v.ripoll@gmail.com <v.ripoll@gmail.com@e025e472-b324-11dd-8bee-0be8dff76351>
2013-05-16 11:33:32 +0000

    AndroidDB v0.7

--- AndroidDB/BusinessObjects/ApplicationSettings.cs
+++ AndroidDB/BusinessObjects/ApplicationSettings.cs
@@ -23,7 +23,7 @@ namespace NRADB.BusinessObjects
 	public static class ApplicationSettings
 	{
 		public static readonly string ApplicationName = "NRADB";
-		public static readonly SoftwareVersion ApplicationVersion = new SoftwareVersion(0, 6, 0);
+		public static readonly SoftwareVersion ApplicationVersion = new SoftwareVersion(0, 7, 0);
 		public static NraDatabaseManager DatabaseManager { get; set; }
 		public static bool ImagesFolderExists { get; set; }
 		public static string ImagesFolder { get; set; }
