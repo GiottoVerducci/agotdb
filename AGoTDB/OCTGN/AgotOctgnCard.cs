8c9afca8ae28e36558000e6dc74dfb10930be00f
v.ripoll@gmail.com <v.ripoll@gmail.com@e025e472-b324-11dd-8bee-0be8dff76351>
2014-04-25 14:58:14 +0000

    Increased tolerance to errors when matching set names or card names. Added support for multiple identical cards with different ids (eg. Carrion Birds), and for different cards with the same name (eg. The Kingsroad)

--- AGoTDB/OCTGN/AgotOctgnCard.cs
+++ AGoTDB/OCTGN/AgotOctgnCard.cs
@@ -3,67 +3,90 @@ using GenericDB.OCTGN;
 
 namespace AGoTDB.OCTGN
 {
-	[DebuggerDisplay("{Name}")]
-	public class AgotOctgnCard: IOctgnCard
-	{
-		[OctgnCardData(AttributeName = "id")]
-		public string Id { get; set; }
+    [DebuggerDisplay("{Name}")]
+    public class AgotOctgnCard : IOctgnCard
+    {
+        [OctgnCardData(AttributeName = "id")]
+        public string Id { get; set; }
 
-		[OctgnCardData(AttributeName = "name")]
-		public string Name { get; set; }
+        [OctgnCardData(AttributeName = "name")]
+        public string Name { get; set; }
 
-		[OctgnCardData(PropertyName = "House")]
-		public string House { get; set; }
+        [OctgnCardData(AttributeName = "House")]
+        public string House { get; set; }
 
-		[OctgnCardData(PropertyName = "Type")]
-		public string Type { get; set; }
+        [OctgnCardData(AttributeName = "Type")]
+        public string Type { get; set; }
 
-		[OctgnCardData(PropertyName = "Traits")]
-		public string Traits { get; set; }
+        [OctgnCardData(AttributeName = "Traits")]
+        public string Traits { get; set; }
 
-		[OctgnCardData(PropertyName = "Text")]
-		public string Text { get; set; }
+        [OctgnCardData(AttributeName = "Text")]
+        public string Text { get; set; }
 
-		[OctgnCardData(PropertyName = "Keywords")]
-		public string Keywords { get; set; }
+        [OctgnCardData(AttributeName = "Keywords")]
+        public string Keywords { get; set; }
 
-		[OctgnCardData(PropertyName = "Unique")]
-		public string Unique { get; set; }
+        [OctgnCardData(AttributeName = "Unique")]
+        public string Unique { get; set; }
 
-		[OctgnCardData(PropertyName = "Cost")]
-		public string Cost { get; set; }
+        [OctgnCardData(AttributeName = "Cost")]
+        public string Cost { get; set; }
 
-		[OctgnCardData(PropertyName = "Strength")]
-		public string Strength { get; set; }
+        [OctgnCardData(AttributeName = "Strength")]
+        public string Strength { get; set; }
 
-		[OctgnCardData(PropertyName = "Military")]
-		public string Military { get; set; }
+        [OctgnCardData(AttributeName = "Military")]
+        public string Military { get; set; }
 
-		[OctgnCardData(PropertyName = "Intrigue")]
-		public string Intrigue { get; set; }
+        [OctgnCardData(AttributeName = "Intrigue")]
+        public string Intrigue { get; set; }
 
-		[OctgnCardData(PropertyName = "Power")]
-		public string Power { get; set; }
+        [OctgnCardData(AttributeName = "Power")]
+        public string Power { get; set; }
 
-		[OctgnCardData(PropertyName = "Crest")]
-		public string Crest { get; set; }
+        [OctgnCardData(AttributeName = "Crest")]
+        public string Crest { get; set; }
 
-		[OctgnCardData(PropertyName = "GoldIncome")]
-		public string GoldIncome { get; set; }
+        [OctgnCardData(AttributeName = "GoldIncome")]
+        public string GoldIncome { get; set; }
 
-		[OctgnCardData(PropertyName = "Influence")]
-		public string Influence { get; set; }
+        [OctgnCardData(AttributeName = "Influence")]
+        public string Influence { get; set; }
 
-		[OctgnCardData(PropertyName = "Initiative")]
-		public string Initiative { get; set; }
+        [OctgnCardData(AttributeName = "Initiative")]
+        public string Initiative { get; set; }
 
-		[OctgnCardData(PropertyName = "PlotGoldIncome")]
-		public string PlotGoldIncome { get; set; }
+        [OctgnCardData(AttributeName = "PlotGoldIncome")]
+        public string PlotGoldIncome { get; set; }
 
-		[OctgnCardData(PropertyName = "PlotInitiative")]
-		public string PlotInitiative { get; set; }
+        [OctgnCardData(AttributeName = "PlotInitiative")]
+        public string PlotInitiative { get; set; }
 
-		[OctgnCardData(PropertyName = "PlotClaim")]
-		public string PlotClaim { get; set; }
-	}
+        [OctgnCardData(AttributeName = "PlotClaim")]
+        public string PlotClaim { get; set; }
+
+        public bool Equals(AgotOctgnCard other)
+        {
+            return this.Name == other.Name
+               && this.House == other.House
+               && this.Type == other.Type
+               && this.Traits == other.Traits
+               && this.Text == other.Text
+               && this.Keywords == other.Keywords
+               && this.Unique == other.Unique
+               && this.Cost == other.Cost
+               && this.Strength == other.Strength
+               && this.Military == other.Military
+               && this.Intrigue == other.Intrigue
+               && this.Power == other.Power
+               && this.Crest == other.Crest
+               && this.GoldIncome == other.GoldIncome
+               && this.Influence == other.Influence
+               && this.Initiative == other.Initiative
+               && this.PlotGoldIncome == other.PlotGoldIncome
+               && this.PlotInitiative == other.PlotInitiative
+               && this.PlotClaim == other.PlotClaim;
+        }
+    }
 }
