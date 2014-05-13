bd16d87d9fabc231a3be4fa180947e2fe544af2b
v.ripoll@gmail.com <v.ripoll@gmail.com@e025e472-b324-11dd-8bee-0be8dff76351>
2014-05-13 15:42:48 +0000

    Updated copyrights. v.750

--- AGoTDB/BusinessObjects/ApplicationSettings.cs
+++ AGoTDB/BusinessObjects/ApplicationSettings.cs
@@ -1,5 +1,5 @@
 // AGoTDB - A card searcher and deck builder tool for the CCG "A Game of Thrones"
-// Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013 Vincent Ripoll
+// Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Vincent Ripoll
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -40,7 +40,7 @@ namespace AGoTDB.BusinessObjects
         }
 
         public readonly string ApplicationName = "AGoTDB";
-        public readonly SoftwareVersion ApplicationVersion = new SoftwareVersion(0, 749 , 0);
+        public readonly SoftwareVersion ApplicationVersion = new SoftwareVersion(0, 750 , 0);
         public AgotDatabaseManager DatabaseManager { get; set; }
         public bool ImagesFolderExists { get; set; }
         public string ImagesFolder { get; set; }
--- GenericDB/DataAccess/QueryBuilder.cs
+++ GenericDB/DataAccess/QueryBuilder.cs
@@ -1,5 +1,5 @@
 // GenericDB - A generic card searcher and deck builder library for CCGs
-// Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013 Vincent Ripoll
+// Copyright © 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014 Vincent Ripoll
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
